d7ccee408914cddcab2ee85d5e7954cafcaac168
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-07-19 12:45:03 +0200

    Initial Commit

---
+++ Subscriber/SubscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriberApp.swift
+//  Subscriber
+//
+//  Created by Skorobogatow, Christian on 19/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SubscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
